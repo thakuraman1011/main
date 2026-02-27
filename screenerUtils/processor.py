7b4b51de6c49415aac1cc6b287cb3077d26db8e3
Aman Thakur <amanthakur@Amans-Mac-mini.local>
2026-02-26 23:11:39 -0800

    tested changes ready to commit

--- screenerUtils/processor.py
+++ screenerUtils/processor.py
@@ -5,7 +5,9 @@ from pathlib import Path
 from datetime import date
 import shutil
 import json
-from screenerUtils.CONFIG import _DATE_THRESHOLD, _COMPANY_FACTS, _MODIFIED_FACTS, _MIN_KEYS_IN_MODIFIED_JSON
+import CONFIG
+import zipfile
+import urllib.request
 
 def _transform_instance(json_data):
   """
@@ -35,7 +37,7 @@ def _transform_instance(json_data):
         continue
       else:
         fact_dictionary ={}
-        if date.fromisoformat(fact['end']) > date.fromisoformat(_DATE_THRESHOLD) and (fact['form'] in ['10-K','10-Q','20-F','6-K']):
+        if date.fromisoformat(fact['end']) > date.fromisoformat(CONFIG._DATE_THRESHOLD) and (fact['form'] in ['10-K','10-Q','20-F','6-K']):
           fact_dictionary ['end'] = fact['end']
           fact_dictionary ['val'] = fact['val']
           fact_dictionary ['form'] = fact['form']
@@ -45,15 +47,17 @@ def _transform_instance(json_data):
           fact_dictionary ['unit'] = key
           fact_list.append(fact_dictionary)
           dup_avoiding_dictionary[fact['end']] = True
-    new_json[element_key] = fact_list
+    # Add element only if there was at least one fact.
+    if len(fact_list) > 0:
+        new_json[element_key] = fact_list
 
-  # if no facts were added drop this file
-  if len(new_json) > _MIN_KEYS_IN_MODIFIED_JSON:
+  # if the only facts are CIK and entityName then return None
+  if len(new_json) > CONFIG._MIN_KEYS_IN_MODIFIED_JSON:
     return new_json
   else:
     return None
 
-def setup_company_facts(url=CONFIG._URL, zip_filename=CONFIG._TEMP_ZIP_FILE, folder_name=CONFIG._COMPANY_FACTS):
+def process_sec_data(url=CONFIG._URL, zip_filename=CONFIG._TEMP_ZIP_FILE, folder_name=CONFIG._COMPANY_FACTS):
     """Download and extract company facts from SEC EDGAR.
     This function performs the following steps:
     1. Deletes the target folder if it already exists to ensure a clean state.
@@ -100,7 +104,7 @@ def setup_company_facts(url=CONFIG._URL, zip_filename=CONFIG._TEMP_ZIP_FILE, fol
     except Exception as e:
         print(f"An error occurred: {e}")
 
-def transform(file_name = None, source_dir=_COMPANY_FACTS, dest_dir=_MODIFIED_FACTS):
+def transform(file_name = None, source_dir=CONFIG._COMPANY_FACTS, dest_dir=CONFIG._MODIFIED_FACTS):
   """
   iterates over all json files in source_dir
   1. Read file
@@ -137,6 +141,3 @@ def transform(file_name = None, source_dir=_COMPANY_FACTS, dest_dir=_MODIFIED_FA
     except Exception as e:
         print(f"Error processing {file_path.name}: {e}")
 
-########################## Execution block ################################
-# transform()
-# print('modified files created')
